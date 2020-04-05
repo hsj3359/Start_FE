6e9553b3f5e830acab1f668fbc94c30e85d37254
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-04-05 22:36:11 +0900

    임시

---
+++ 02/app.js
@@ -0,0 +1,9 @@
+ var count = [10,20,30,40,50,60,70,100];
+var sum =0;
+ for(var num =0; num < count.lenth; num++){
+   sum += count[num];
+   if(num == count.lenth-1){
+     print(sum);
+   }
+ }
+print(sum);
