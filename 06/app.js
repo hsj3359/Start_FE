8b6a6bbe090c86da9b4f9e269e9125d62c4c985c
hsj3359 <37091550+hsj3359@users.noreply.github.com>
2020-05-02 13:49:32 +0900

    실습

---
+++ 06/app.js
@@ -0,0 +1,13 @@
+var age = 20;
+console.log(age);
+var bol = true;
+console.log(bol);
+var month = new Array();
+month = [1,2,3,4,5,6,7,8,9,10,11,12];
+console.log(month);
+var host = {
+    nick: 'sungjun',
+    age: 20,
+    area: 'jeju'
+  };
+  console.log(host);
